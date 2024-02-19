2eae44de7c9f04e6c644d5850e1b853d4f5316cf
Fredrik Thomsson <fred@MBPsomtrFredrik.lan>
2024-02-19 10:30:48 +0100

    fixed bug

--- freddeselektronik/src/Components/AllProducts/AllProducts.jsx
+++ freddeselektronik/src/Components/AllProducts/AllProducts.jsx
@@ -1,24 +1,32 @@
+// AllProducts.jsx
 import React from 'react';
 import './AllProducts.css';
-import all_products from '../Assets/all_product'; // Assuming your data is in a separate file
+import all_product from '../Assets/all_product'; // Assuming your data is in a separate file
 import Item from '../Item/Item';
 
-const AllProducts = () => {
+const AllProducts = ({ category }) => {
   // Assuming your product data is imported directly or obtained from an API
-  const allProducts = all_products;
+  const all_products = all_product;
+
+  // Filter products based on the specified category
+  const filteredProducts = category
+    ? all_products.filter((product) => product.category && product.category.includes(category))
+    : all_products;
 
   return (
     <div className="products">
-      {allProducts.map((item, i) => (
-        <Item
-          key={i}
-          id={item.id}
-          name={item.name}
-          image={item.image}
-          new_price={item.new_price}
-          old_price={item.old_price}
-        />
-      ))}
+      <div className="product-item">
+        {filteredProducts.map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
+      </div>
     </div>
   );
 };
