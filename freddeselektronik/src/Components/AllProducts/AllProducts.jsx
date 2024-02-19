94f202978869ba3eb420aa85835058991d3d2633
Fredrik Thomsson <fred@MBPsomtrFredrik.lan>
2024-02-19 10:27:07 +0100

    fixed product layout

--- freddeselektronik/src/Components/AllProducts/AllProducts.jsx
+++ freddeselektronik/src/Components/AllProducts/AllProducts.jsx
@@ -1,32 +1,24 @@
-// AllProducts.jsx
 import React from 'react';
 import './AllProducts.css';
-import all_product from '../Assets/all_product'; // Assuming your data is in a separate file
+import all_products from '../Assets/all_product'; // Assuming your data is in a separate file
 import Item from '../Item/Item';
 
-const AllProducts = ({ category }) => {
+const AllProducts = () => {
   // Assuming your product data is imported directly or obtained from an API
-  const all_products = all_product;
-
-  // Filter products based on the specified category
-  const filteredProducts = category
-    ? all_products.filter((product) => product.category && product.category.includes(category))
-    : all_products;
+  const allProducts = all_products;
 
   return (
     <div className="products">
-      <div className="product-item">
-        {filteredProducts.map((item, i) => (
-          <Item
-            key={i}
-            id={item.id}
-            name={item.name}
-            image={item.image}
-            new_price={item.new_price}
-            old_price={item.old_price}
-          />
-        ))}
-      </div>
+      {allProducts.map((item, i) => (
+        <Item
+          key={i}
+          id={item.id}
+          name={item.name}
+          image={item.image}
+          new_price={item.new_price}
+          old_price={item.old_price}
+        />
+      ))}
     </div>
   );
 };
