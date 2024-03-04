772e8ed43fe2227b4e0ba318256bf5e296ad5f04
Fredrik Thomsson <fred@MBPsomtrFredrik.lan>
2024-03-04 10:01:50 +0100

    product display design changes

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import './ProductDisplay.css'
 import { ShopContext } from '../../Context/ShopContext';
+import Image from 'react-bootstrap/Image';
 
 const ProductDisplay = (props) => {
   const {product} = props;
@@ -8,28 +9,24 @@ const ProductDisplay = (props) => {
   return (
     <div className='productdisplay'>
       <div className="productdisplay-left">
-
-        <div className="productdisplay-img">
-          <img className='productdisplay-main-img' src={product.image} alt="" />
-        </div>
+        <Image src={product.image} height={400} width={500} alt="" />
       </div>
       <div className="productdisplay-right">
         <h1>{product.name}</h1>
         <p>{product.description}</p>
         <div className="productdisplay-right-prices">
-        <div className="productdisplay-right-price-old">
-              {props.old_price !== null && props.old_price !== undefined && (
-                <>
-                  {props.old_price}kr
-                </>
-              )}
-              </div>
-        <div className="productdisplay-right-price-new">{product.new_price}kr </div>
+          <div className="productdisplay-right-price-old">
+            {product.old_price !== null && product.old_price !== undefined && (
+              <>
+                {product.old_price}kr
+              </>
+            )}
+          </div>
+          <div className="productdisplay-right-price-new">{product.new_price}kr </div>
         </div>
         <div className="productdisplay-right-description"></div>
         <button onClick={()=>{addToCart(product.id)}}>Add To Cart</button>
       </div>
-      
     </div>
   )
 }
