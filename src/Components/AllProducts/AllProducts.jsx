168f1f5adc5642052d102b71ea0fb3a268a94111
Fredrik Thomsson <fred@MBPsomtrFredrik.lan>
2024-05-21 09:09:16 +0200

    made home page responsive

--- src/Components/AllProducts/AllProducts.jsx
+++ src/Components/AllProducts/AllProducts.jsx
@@ -1,33 +1,50 @@
 import React from 'react';
 import './AllProducts.css';
-import all_product from '../Assets/all_product'; // Assuming your data is in a separate file
+import all_product from '../Assets/all_product';
 import Item from '../Item/Item';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const AllProducts = ({ category }) => {
-  // Assuming your product data is imported directly or obtained from an API
   const all_products = all_product;
-
-  // Filter products based on the specified category
   const filteredProducts = category
     ? all_products.filter((product) => product.category && product.category.includes(category))
     : all_products;
 
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 1024 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 1024, min: 768 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 768, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
-
-    <div className="products container">
-      {/* Render each product item directly inside the .products container */}
-      {filteredProducts.map((item, i) => (
-        <Item
-          key={i}
-          id={item.id}
-          name={item.name}
-          image={item.image}
-          new_price={item.new_price}
-          old_price={item.old_price}
-        />
-      ))}
+    <div className="products">
+      <Carousel responsive={responsive} infinite={true} autoPlay={false} autoPlaySpeed={3000} itemClass="carousel-item-padding" >
+        {filteredProducts.map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
+      </Carousel>
     </div>
-
   );
 };
 
