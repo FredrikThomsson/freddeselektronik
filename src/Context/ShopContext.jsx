3181d38dcfe566c02fca7eee53d563ed8fcffd6f
Fredrik Thomsson <fred@MBPsomtrFredrik.lan>
2024-05-22 20:22:39 +0200

    added snackbar notification when adding an item to the cart

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,35 +1,45 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useContext } from "react";
+import { Snackbar } from "@mui/material";
 import all_product from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
-    let cart = {}
-    for (let index = 0; index < all_product.length+1; index++) {
-        cart[index] = 0; 
-     }
-     return cart;
+    let cart = {};
+    for (let index = 0; index < all_product.length + 1; index++) {
+        cart[index] = 0;
+    }
+    return cart;
 }
 
 const ShopContextProvider = (props) => {
-    const [cartItems,setCartItems] = useState(getDefaultCart());
-    
+    const [cartItems, setCartItems] = useState(getDefaultCart());
+    const [showSnackbar, setShowSnackbar] = useState(false);
+
     const addToCart = (itemId) => {
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
-    } 
-    const removeAll = (itemId) => {
-        setCartItems((prev) => ({...prev, [itemId]: 0}));
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+        setShowSnackbar(true);
     };
 
-    const removeFromCart = (itemId) => {
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
-    } 
-    const contextValue = {all_product,cartItems,addToCart,removeFromCart,removeAll}
+    const handleCloseSnackbar = () => {
+        setShowSnackbar(false);
+    };
+
+    const contextValue = { all_product, cartItems, addToCart };
+
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
+            <Snackbar
+                open={showSnackbar}
+                autoHideDuration={3000}
+                onClose={handleCloseSnackbar}
+                message="Item added to cart!"
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }} 
+                
+            />
         </ShopContext.Provider>
-    )
+    );
 }
 
 export default ShopContextProvider;
